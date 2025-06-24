d4250a44561f972b854d894aa2d61871821ef85c
Kirill <mustang.horse34@gmail.com>
2025-06-24 13:34:53 +0700

    update bestNewMovies component

--- src/widgets/bestNewMovies/ui/bestNewMovies.tsx
+++ src/widgets/bestNewMovies/ui/bestNewMovies.tsx
@@ -8,7 +8,16 @@ export const BestNewMovies = async () => {
     sortField: ['votes.kp', 'rating.kp'],
     sortType: ['-1', '-1'],
     'releaseYears.start': '2024',
-    selectFields: ['poster', 'name', 'id', 'year', 'movieLength', 'rating'],
+    selectFields: [
+      'poster',
+      'name',
+      'id',
+      'year',
+      'movieLength',
+      'rating',
+      'seriesLength',
+      'isSeries',
+    ],
     notNullFields: ['poster.url', 'name'],
   });
 
