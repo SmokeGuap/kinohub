c1d337f0acf2f3bb41136c073fe62b4f46a083a9
Kirill <mustang.horse34@gmail.com>
2025-06-26 11:33:55 +0700

    add SortingPanel

---
+++ src/features/sortingPanel/ui/sortingPanel.tsx
@@ -0,0 +1,17 @@
+import { genres, ratings, sorting, years } from '@/shared/config/filtersData';
+import { Dropdown } from '../../../shared/ui/dropdown';
+
+export const SortingPanel = () => {
+  return (
+    <div className='flex flex-wrap gap-4 mb-8'>
+      <Dropdown name={{ label: 'Жанры', name: 'genre' }} data={genres} />
+      <Dropdown name={{ label: 'Рейтинг', name: 'rating' }} data={ratings} />
+      <Dropdown name={{ label: 'Годы', name: 'year' }} data={years} />
+      <Dropdown
+        name={{ label: 'Сортировка', name: 'sort' }}
+        data={sorting}
+        alignRight
+      />
+    </div>
+  );
+};
