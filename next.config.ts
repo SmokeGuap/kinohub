138b0d502b283fe18aeca92ad8f090e3411767ba
Kirill <mustang.horse34@gmail.com>
2025-07-02 18:36:32 +0700

    add jest and testing library config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  transpilePackages: ['swiper'],
   images: {
     remotePatterns: [
       new URL('https://image.openmoviedb.com/**'),
